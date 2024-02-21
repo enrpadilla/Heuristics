142b8406cc210f143ba4ad46b7c5b13b83a5f824
enrpadilla <enrique.padilla.isw@gmail.com>
2024-02-20 23:23:49 -0600

    Creation of Julia module.

---
+++ Heuristics.jl
@@ -0,0 +1,20 @@
+module TSPHeuristics
+    using LinearAlgebra
+
+    """
+        nearest(numcities::Int,startingcity::Int,distmat::Matrix{Real})
+    
+    Return a solution to solve the TSP using the nearest neighbor algorithm.
+
+    # Examples
+    ```julia-repl
+    julia> nearest(4,2,[])
+    ```
+    """ 
+    function nearest(numcities::Int,startingcity::Int,distmat::Matrix{Real})
+        
+
+
+    end
+
+end
