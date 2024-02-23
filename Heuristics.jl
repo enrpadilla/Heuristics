476f748328a323abc7c58abb9110f5af5208ebd8
enrpadilla <enrique.padilla.isw@gmail.com>
2024-02-22 23:31:49 -0600

    Updates to nearest neighbor algorithm

--- Heuristics.jl
+++ Heuristics.jl
@@ -11,10 +11,26 @@ module TSPHeuristics
     julia> nearest(4,2,[])
     ```
     """ 
-    function nearest(numcities::Int,startingcity::Int,distmat::Matrix{Real})
-        
+    function nearest(numcities::Int,startingcity::Int,distmat::AbstractMatrix{T} where {T<:Real})
+        issquare = LinearAlgebra.checksquare(distmat)
+        diagonal = diag(distmat)
+        nanmatrix = fill(NaN,numcities,1)
 
-
+        # Validate user inputs
+        if (numcities <= 0)
+            throw(DomainError(numcities,"Number of cities must be a positive integer."))
+        elseif (startingcity <= 0 || startingcity > numcities)
+            throw(DomainError(startingcity,"The starting city must be a positive integer and less than or equal to the number of cities."))
+        elseif (numcities != issquare)   
+            throw(DomainError(distmat,"Matrix must be square and dimensions must be equal to number of cities."))
+        elseif (diagonal != nanmatrix)
+            throw(DomainError(diagonal,"The diagonal must consist of NaN values."))
+        else
+            # Nearest neighbor algorithm implementation
+            
+        end
     end
-
 end
+
+solution = TSPHeuristics.nearest(2,2,[2 2;1 1])
+print(solution)
